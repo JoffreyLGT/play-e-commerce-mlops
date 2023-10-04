71459747c8b988ef04e1812c54663f3233a864fe
Joffrey Lagut <j.lagut@live.fr>
2023-10-04 10:48:08 +0200

    chore: remove commitlint reference rule
    Since our Git workflow is based on squasing the commits when merging a PR,
    we only need to provide the references in the last commit.

--- commitlint.config.js
+++ commitlint.config.js
@@ -3,6 +3,5 @@ module.exports = {
     rules: {
         "header-max-length": [2, "always", 72],
         "body-max-line-length": [2, "always", 100],
-        "references-empty": [2, "never"],
     }
 }
